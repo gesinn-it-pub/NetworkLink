9c13519c6d13de85881bfbf526613d952ff85037
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-11-11 15:36:19 +0100

    fix sniff Squiz.WhiteSpace.SuperfluousWhitespace reported by phpcs

--- tests/phpunit/Unit/NetworkLinkTest.php
+++ tests/phpunit/Unit/NetworkLinkTest.php
@@ -20,7 +20,7 @@ class NetworkLinkTest extends TestCase {
 
     protected function setUp(): void {
         parent::setUp();
-        
+
         // Mock the Parser class
         $this->parser = $this->createMock( Parser::class );
     }
@@ -29,9 +29,9 @@ class NetworkLinkTest extends TestCase {
      * Test <link> tag with path and title attributes.
      */
     public function testLinkTagWithPathAndTitle() {
-        $output = NetworkLink::renderNetworkLink( 'Example Link', 
-                                                [ 'path' => 'https://example.com', 'title' => 'Example Site' ], 
-                                                $this->parser, 
+        $output = NetworkLink::renderNetworkLink( 'Example Link',
+                                                [ 'path' => 'https://example.com', 'title' => 'Example Site' ],
+                                                $this->parser,
                                                 $this->createMock( PPFrame::class ) );
 
         $expected = '<a href="https://example.com" target="_blank" title="Example Site">Example Site</a>';
@@ -42,9 +42,9 @@ class NetworkLinkTest extends TestCase {
      * Test <link> tag without any attributes.
      */
     public function testLinkTagWithoutAttributes() {
-        $output = NetworkLink::renderNetworkLink( 'https://example.com', 
-                                                [], 
-                                                $this->parser, 
+        $output = NetworkLink::renderNetworkLink( 'https://example.com',
+                                                [],
+                                                $this->parser,
                                                 $this->createMock( PPFrame::class ) );
 
         $expected = '<a href="https://example.com" target="_blank" title="https://example.com">https://example.com</a>';
