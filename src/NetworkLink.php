aa923ee85293a044896ac6196bafbeca77a30499
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-11-11 11:17:19 +0100

    update NetworkLink.php
    - rewrite the render function

--- src/NetworkLink.php
+++ src/NetworkLink.php
@@ -9,33 +9,49 @@ use MediaWiki\MediaWikiServices;
 class NetworkLink {
 
     /**
-     * Register the <networklink> tag during the parser setup
+     * Register the <link> tag during the parser setup
      */
     public static function onParserFirstCallInit( Parser $parser ) {
-        // Register the tag hook for <networklink>
-        $parser->setHook( 'networklink', [ __CLASS__, 'networklink' ] );
+        // Register the tag hook for <link>
+        $parser->setHook( 'link', [ __CLASS__, 'renderNetworkLink' ] );
     }
 
     /**
-     * Callback function to handle <networklink> tag
+     * Callback function to handle <link> tag
      *
-     * @param string $input Content inside the <networklink> tag
+     * @param string $input Content inside the <link> tag
      * @param array $args Arguments passed to the tag
      * @param Parser $parser The parser object
      * @param PPFrame $frame The parser frame
      * @return string Rendered HTML link
      */
-    public static function networklink( $input, array $args, Parser $parser, PPFrame $frame ) {
+    public static function renderNetworkLink( $input, array $args, Parser $parser, PPFrame $frame ) {
         // Extract the 'path' argument if present, otherwise use the input text
         $path = isset( $args['path'] ) ? $args['path'] : $input;
 
         // Sanitize the file path to prevent any security issues
         $sanitizedPath = htmlspecialchars( $path, ENT_QUOTES, 'UTF-8' );
 
-        // Use $input for the link text
-        $linkText = $input ?: "Open File";  // Default to "Open File" if no input is provided
+        // Use the 'title' argument if present, otherwise default to the link path
+        $linkTitle = isset( $args['title'] ) ? htmlentities( $args['title'], ENT_QUOTES, 'UTF-8' ) : $input;
 
-        // Return an HTML link that opens the file with a 'file://' protocol
-        return "<a href='file://$sanitizedPath' target='_blank'>$linkText</a>";
+        // Set the target behavior (default to "_blank")
+        $target = isset( $args['target'] ) ? strtoupper( $args['target'] ) : 'BLANK';
+        switch ( $target ) {
+            case "SELF":
+                $targetAttr = "_self";
+                break;
+            case "TOP":
+                $targetAttr = "_top";
+                break;
+            case "PARENT":
+                $targetAttr = "_parent";
+                break;
+            default:
+                $targetAttr = "_blank";
+        }
+
+        // Return the HTML link with the appropriate target and title
+        return sprintf( '<a href="file://%1$s" target="%2$s" title="%3$s">%3$s</a>', $sanitizedPath, $targetAttr, $linkTitle );
     }
 }
